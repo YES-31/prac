2d7e4e935b0e3031c78df8f4545e2fb10ebb1f3d
origin <yesk31@naver.com>
2023-03-13 18:39:27 +0900

    fix: 누락 추가

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -6,12 +6,19 @@ class CartController {
   //장바구니 상품 추가하기
   addItemInCart = async (req, res) => {
     try {
+      const userId = 1;
+      const { itemId } = req.params;
 
+      const addItem = await this.cartService.addItemInCart(itemId, userId);
 
+      return res.status(200).json({
+        data: addItem,
+        message: "장바구니 추가 완료",
+        result: true,
+      });
     } catch (err) {
-        console.log(err);
-        return res.status(400).JSON({errormessage: 'controller단 오류'})
-
+      console.log(err);
+      return res.status(400).JSON({ errormessage: "controller단 오류" });
     }
   };
 }
