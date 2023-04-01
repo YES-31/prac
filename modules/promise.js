3e2b4b636119e3966c9ad8aceafc697c5e90c0a0
origin <yesk31@naver.com>
2023-04-01 19:16:20 +0900

    promise resolve

--- modules/promise.js
+++ modules/promise.js
@@ -1,14 +1,15 @@
 const timerPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log("1st");
-    resolve("data info"); // promise.then 인자로 들어감
+    resolve("data info", "data2"); // promise.then 인자로 들어감
   }, 1000);
 });
 
-timerPromise.then((data) => {
+timerPromise.then((data, data2) => {
   console.log("2st");
   console.log("3st");
 
   //resolve에 들어간 인자값을 받아서 사용
-  console.log(data)
+  console.log(data); // "data info"
+  console.log(data2); // "undefined"  한 개의 인자만 사용 가능한 것인가?
 });
