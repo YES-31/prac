faa4b3e8660e97ad5e609db91500661f1bfa1136
origin <yesk31@naver.com>
2023-04-01 19:14:34 +0900

    resolve then 인자 사용

--- modules/promise.js
+++ modules/promise.js
@@ -1,11 +1,14 @@
 const timerPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log("1st");
-    resolve();
+    resolve("data info"); // promise.then 인자로 들어감
   }, 1000);
 });
 
-timerPromise.then(() => {
+timerPromise.then((data) => {
   console.log("2st");
   console.log("3st");
+
+  //resolve에 들어간 인자값을 받아서 사용
+  console.log(data)
 });
