8c5598fdb467c550fec50eb1eb49943adad04dd0
origin <yesk31@naver.com>
2023-04-01 19:19:43 +0900

    promise than_catch

--- modules/promise.js
+++ modules/promise.js
@@ -1,15 +1,20 @@
 const timerPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log("1st");
-    resolve("data info", "data2"); // promise.then 인자로 들어감
+    //resolve("data info", "data2"); // promise.then 인자로 들어감
+    reject("error");
   }, 1000);
 });
 
-timerPromise.then((data, data2) => {
-  console.log("2st");
-  console.log("3st");
+timerPromise
+  .then((data, data2) => {
+    console.log("2st");
+    console.log("3st");
 
-  //resolve에 들어간 인자값을 받아서 사용
-  console.log(data); // "data info"
-  console.log(data2); // "undefined"  한 개의 인자만 사용 가능한 것인가?
-});
+    //resolve에 들어간 인자값을 받아서 사용
+    console.log(data); // "data info"
+    console.log(data2); // "undefined"  한 개의 인자만 사용 가능한 것인가?
+  })
+  .catch((err) => {
+    console.log("promise에서 에러 발생 !", err);
+  });
