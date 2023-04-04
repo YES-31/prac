77c239677b4aec7f9abaefd0a516c191a471c652
origin <yesk31@naver.com>
2023-04-04 20:28:10 +0900

    chore: dotenv 추가

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+require("dotenv").config();
+
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PW,
+  database: process.env.DB,
+});
+
+connection.connect();
 
 // router 미들웨어를 사용하겠다 선언
 const sampleRouter = require("./router/sample.js");
