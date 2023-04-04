1c559a8034280c1bd2510c6be02f78b8770deb38
origin <yesk31@naver.com>
2023-04-04 21:08:55 +0900

    feat: 에러핸들링

--- app.js
+++ app.js
@@ -3,17 +3,6 @@ const app = express();
 const port = 3000;
 require("dotenv").config();
 
-const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PW,
-  database: process.env.DB,
-});
-
-connection.connect();
-
 // router 미들웨어를 사용하겠다 선언
 const sampleRouter = require("./router/sample.js");
 app.use("/sample", [sampleRouter]);
