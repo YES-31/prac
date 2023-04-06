833dd29c52310ec454554e88aaf3adb98accf514
origin <yesk31@naver.com>
2023-04-06 21:43:26 +0900

    mysql connect

---
+++ repositories/mysqlPrac.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+connection.connect();
+
+connection.query("SELECT * FROM testb", function (error, results, fields) {
+  if (error) throw error;
+  console.log("The solution is: ", results);
+});
+
+connection.end();
