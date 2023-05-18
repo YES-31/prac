8263d2889d41242042643d044b7e629757737626
origin <yesk31@naver.com>
2023-05-18 21:27:01 +0900

    feat: post router

--- router/foodTruckRoutes.js
+++ router/foodTruckRoutes.js
@@ -5,5 +5,5 @@ const FoodTruckController = require("../controllers/foodTruckController");
 const foodTruckController = new FoodTruckController();
 
 router.get("/:truckId", foodTruckController.getTruck);
-
+router.post("/:truckId", foodTruckController.postTruck )
 module.exports = router;
