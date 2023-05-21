152a4e60f0b5920b8090398282133737372568a8
origin <yesk31@naver.com>
2023-05-21 23:54:37 +0900

    test: post service

--- services/foodTruckService.js
+++ services/foodTruckService.js
@@ -6,7 +6,7 @@ class foodTruckService {
   getTruck = async (truckId) => {
     try {
       const result = await this.foodTruckRepositories.getTruck(truckId);
-      console.log("Service asdfasdfasdfasdfasdf", result);
+      console.log("Service result: ", result);
       return result;
     } catch (err) {
       console.log(err);
@@ -16,15 +16,15 @@ class foodTruckService {
     }
   };
 
-  postTurck = async () => {
-    try{
-
-    }catch (err){
-        console.log(err);
-        res.status(400).json({errorMessge: "postTruck error"})
+  postTurck = async (truckId) => {
+    try {
+      const result = await this.foodTruckRepositories.postTruck(truckId);
+      return result;
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({ errorMessge: "postTruck error" });
     }
-
-  }
+  };
 }
 
 module.exports = foodTruckService;
